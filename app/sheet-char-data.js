5a2c2ddf4fd6d389654f3a8aa0f01e7a4ddec9bd
Raman <33904740+Rmnlly@users.noreply.github.com>
2019-05-06 22:08:16 -0400

    updated data source to v3.0, added data transforms roughly, example output in index.js for reference

--- app/sheet-char-data.js
+++ app/sheet-char-data.js
@@ -1,19 +1,25 @@
 let { google } = require("googleapis");
 
+//1k64M9MZ_-s-VDzdK8wrQvzJZJv1HKMusAIZLKhad5cQ is my version 3.0.0 sheet
+
 const getCharacterSheetData = (auth, sheetName) => {
   const sheets = google.sheets({ version: "v4", auth });
 
   return new Promise((resolve, reject) => {
     sheets.spreadsheets.values.get(
       {
-        spreadsheetId: "1ZF5URHAeAuawxCKhL1QPI_iSrjHHexRIUTvJtQ2c4Rk",
+        spreadsheetId: "1k64M9MZ_-s-VDzdK8wrQvzJZJv1HKMusAIZLKhad5cQ",
         range: `${sheetName}!A:J`
       },
       (err, res) => {
         if (err) return console.log("The api returned error: " + err);
         const rawData = res.data.values;
+        //rawData is an array of arrays, the inside values are
+        // from value 0, 1, 2, 3...
+        //[Move Name, startup, total frames, landing lag, additional notes, base damage, shieldlag. shield stun, which hitbox, advantage]
+
         if (rawData.length) {
-          resolve(rawData.map(move => move[0]));
+          resolve(rawData);
         } else {
           resolve([]);
         }
--- app/sheet-char-names.js
+++ app/sheet-char-names.js
@@ -1,11 +1,13 @@
 let { google } = require("googleapis");
 
+//1k64M9MZ_-s-VDzdK8wrQvzJZJv1HKMusAIZLKhad5cQ is my version 3.0.0 sheet
+
 const getSheetNames = auth => {
   const sheets = google.sheets({ version: "v4", auth });
   return new Promise((resolve, reject) => {
     sheets.spreadsheets.get(
       {
-        spreadsheetId: "1ZF5URHAeAuawxCKhL1QPI_iSrjHHexRIUTvJtQ2c4Rk"
+        spreadsheetId: "1k64M9MZ_-s-VDzdK8wrQvzJZJv1HKMusAIZLKhad5cQ"
       },
       (err, res) => {
         resolve(res.data.sheets.map(sheet => sheet.properties.title));
